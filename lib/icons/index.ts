324bcfaae8960cb3a7e444b0de48fbf748535e2a
gwatkins2090 <133011172+gwatkins2090@users.noreply.github.com>
2025-03-06 14:33:47 -0600

    Create index.ts
    Added index.ts to /lib/icons/index.ts

---
+++ lib/icons/index.ts
@@ -0,0 +1,6 @@
+// lib/icons/index.ts
+
+// Export all components and utilities
+export * from './Icons';
+export * from './iconContext';
+export * from './utils'; // This should work if utils.ts exists in the same directory
